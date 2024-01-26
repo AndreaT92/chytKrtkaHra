71b1650ba9272193e628842455f49b48877666f8
Andrea Tauberová <andrea.tauberova@seznam.cz>
2024-01-26 16:13:22 +0100

    Add project files.

---
+++ Hra.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chytKrtka
+{
+    internal class Hra
+    {
+        private int casovyLimit;
+
+        public Hra(int casovyLimit)
+        {
+            this.casovyLimit = casovyLimit;
+        }
+
+        public void SpustSe()
+        { 
+        Krtek krtek = new Krtek();
+        Hrac hrac = new Hrac();
+            Console.WriteLine("Začíná hra chyť krtka (čísla 0 až 9)!");
+            Console.WriteLine($"Máš {casovyLimit} sekund na stisknutí co nejvíce čísel.");
+            DateTime zacatek = DateTime.Now;
+            DateTime konec = zacatek.AddSeconds(casovyLimit);
+
+            int pocetStisknuti = 0;
+            while (DateTime.Now < konec)
+            {
+                int nahodneCislo = krtek.ObjevSe();
+                int zasah = hrac.Zasahni();
+            }
+
+
+        
+                }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace chytKrtka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int casovyLimit = 30;
+            Hra hra = new Hra(casovyLimit);
+            hra.SpustSe();
+        }
+    }
+}
